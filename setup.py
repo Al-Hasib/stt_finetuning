c09b84e967d873d12526fa95a7e72518fd8d8b7b
@jackie.wang <mdabdullahalhasib1111@gmail.com>
2024-12-18 22:21:18 +0600

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 import os
-os.system("!pip install --upgrade pip")
-os.system("pip install -r requirements.txt")
+import schedule
+import time
+
+def run():
+    os.system("pip install --upgrade pip")
+    os.system("pip install -r requirements.txt")
+    os.system("python train.py")
+
+schedule.every().day.at("00:00").do(run)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
